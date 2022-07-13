7099956b541c8a9dcfa1f26599c0c65fc73f6621
ricky <ricky@gmail.com>
2022-07-13 14:42:56 +0000

    hello.java

---
+++ hello.java
@@ -0,0 +1,12 @@
+// Java program to illustrate for loop
+class forLoopDemo {
+    public static void main(String args[])
+    {
+        // Writing a for loop
+        // to print Hello World 5 times
+        for (int i = 1; i <= 5; i++)
+            System.out.println("Hello World");
+    }
+}
+
+
